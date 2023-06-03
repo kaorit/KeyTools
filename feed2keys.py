e04d684157bccb6b5ac075b9d0afa5f3312bb493
Kaori <katanno@gmail.com>
2023-06-03 18:52:38 +0900

    Revert "Update feed2keys.py"
    This reverts commit e6b1cde2c513bd4fa664d19672219aa4b1e33e54.

--- feed2keys.py
+++ feed2keys.py
@@ -1,6 +1,6 @@
 #######################################
 #
-# feed2keys.py v1.0.0-1
+# feed2keys.py v1.0.0
 # ActivityPub @kaori@cal.vino.blue
 #
 #######################################
@@ -8,21 +8,17 @@
 import feedparser
 import textwrap
 import time
-import re
 from misskey import Misskey
 
 #mk = Misskey("yourdomain.ltd", i="API_Key")
-#d = feedparser.parse('https://www,domain.ltd/rss')
-d = feedparser.parse('https://feed.mdpr.jp/rss/export/mdpr-entertainment.xml')
-#print(d.keys())
+d = feedparser.parse('https://www,domain.ltd/rss')
 
 for e in d['entries']:
    string = textwrap.dedent('''
-   {update}
    【{titles}】
    {summaries}
    
    {links}
-   ''').format(update=e['published'], titles=e['title'], summaries=e['summary'], links=e['link']).strip()
+   ''').format(titles=e['title'], summaries=e['summary'], links=e['link']).strip()
 #   mk.notes_create(text=string, visibility='followers')
    print(string)
